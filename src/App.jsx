43c920cd6425020e7325f1c3526db233de76e405
FabianaResstel <fabianaresstel01@gmail.com>
2026-09-13 19:30:23 -0400

    set up the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to the Dictionary App</h1>
+      <h2> What word are you curious about?</h2>
+    </div>
+  );
+}
+
+export default App;
